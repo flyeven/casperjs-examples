04cc0e25429dd8d0281bec077751cafea7ae523b
David Arroyo Menéndez <davidam@es.gnu.org>
2016-01-12 16:05:45 +0100

    davidam-test.js: another way to test links

--- davidam-test.js
+++ davidam-test.js
@@ -2,6 +2,8 @@
  * davidam-test.js - Homepage tests.
  */
 
+var x = require('casper').selectXPath;
+
 casper.test.begin('Tests homepage structure', 7, function suite(test) {
 
   casper.start('http://www.davidam.com', function() {
@@ -9,6 +11,29 @@ casper.test.begin('Tests homepage structure', 7, function suite(test) {
     test.assertExists('a.org-effectiveness', '"Org Effectiveness" link is found.');
     test.assertExists('a.drupal-el', '"Drupal el" link is found.');
     test.assertExists('a.orgmode-drupal', '"Orgmode Drupal" link is found.');
+
+    casper.waitForSelector(x("//*[normalize-space(text())='Traducción de una Introducción a GCC']"),
+       function success() {
+           test.assertExists(x("//*[normalize-space(text())='Traducción de una Introducción a GCC']"));
+         },
+       function fail() {
+           test.assertExists(x("//*[normalize-space(text())='Traducción de una Introducción a GCC']"));
+       });
+    casper.waitForSelector(x("//*[normalize-space(text())='fuentes']"),
+       function success() {
+           test.assertExists(x("//*[normalize-space(text())='fuentes']"));
+       },
+       function fail() {
+           test.assertExists(x("//*[normalize-space(text())='fuentes']"));
+       });
+    casper.waitForSelector(x("//*[normalize-space(text())='Traducción de Guía Compacta de Org Mode']"),
+       function success() {
+           test.assertExists(x("//*[normalize-space(text())='Traducción de Guía Compacta de Org Mode']"));
+         },
+       function fail() {
+           test.assertExists(x("//*[normalize-space(text())='Traducción de Guía Compacta de Org Mode']"));
+       });
+
     // 10 articles should be listed.
     test.assertElementCount('.box', 10, '10 boxes are listed.');
   });
